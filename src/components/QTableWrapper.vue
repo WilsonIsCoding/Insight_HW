0a4880a6ed63fd14eb0fd8b5919911e3e6410037
WilsonIsCoding <wilsoncheng0704.wc@gmail.com>
2024-01-01 13:57:00 +0800

    [✨ feat]：code migration

--- src/components/QTableWrapper.vue
+++ src/components/QTableWrapper.vue
@@ -7,15 +7,35 @@
     row-key="name"
     selection="multiple"
     v-model:selected="tableSelected"
-  ></q-table>
+  >
+    <template v-slot:header-selection="scope">
+      <q-checkbox v-model="scope.selected" />
+    </template>
+    <template v-slot:body="props">
+      <q-tr :props="props">
+        <q-td>
+          <q-checkbox v-model="props.selected" />
+        </q-td>
+        <QtdCore title="name" :props="props" />
+        <QtdCore title="cellphone" :props="props" />
+        <QtdCore title="email" :props="props" />
+        <QtdCore title="gender" :props="props" />
+        <QtdCore title="birthday" :props="props" />
+      </q-tr>
+    </template>
+  </q-table>
 </template>
 
 <script>
 import { ref, watch } from "vue";
+import QtdCore from "./core/QtdCore.vue";
 export default {
   props: {
     data: Object,
     selected: Array,
+  },
+  components: {
+    QtdCore,
   },
   setup(props, { emit }) {
     const tableData = ref(props.data);
---
+++ src/components/core/QtdCore.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-td :key="title" :props="props">
+    <q-tooltip anchor="bottom middle" self="bottom middle">
+      {{ props.row[title] }}
+    </q-tooltip>
+    {{ props.row[title] }}
+  </q-td>
+</template>
+
+<script>
+export default {
+  props: {
+    props: Object,
+    title: String,
+  },
+};
+</script>
